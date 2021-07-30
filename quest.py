f398df88e684c7ee90e08251ef713bd19015e371
Mamata720 <87574644+Mamata720@users.noreply.github.com>
2021-07-30 15:50:20 +0530

    Add files via upload

---
+++ quest.py
@@ -0,0 +1,10 @@
+a=["mamta","maynak","mohan","priya","pooja"]
+num=input("input any alaphabte")
+i=0
+while i<len(a):
+    j=0
+    while j<1:
+        if a[i][j]==num:
+            print(a[i])
+        j=j+1
+    i=i+1
